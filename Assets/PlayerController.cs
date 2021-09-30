48eda53f27dedea9c94459a41999c3aa3e4742b9
Dan <danpaul309@gmail.com>
2021-09-30 15:08:28 +0100

    Check-in updates + lowering backwards speed automatically

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -18,24 +18,16 @@ public class PlayerController : MonoBehaviour
     private Vector3 m_left = new Vector3(0, 0, 0);
     private readonly Vector3 m_gravity = new Vector3(0, -9.8f, 0);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //transform.Rotate(0,20,0);
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (m_moveForward)
         {
-            //rb.AddForce(transform.right * m_speed * Time.fixedDeltaTime * 100, ForceMode.Force);
             m_characterController.Move(transform.right * m_speed * Time.deltaTime);
         }
         else if (m_moveBackward)
         {
-            //rb.AddForce(-transform.right * m_speed * Time.fixedDeltaTime * 100, ForceMode.Force);
-            m_characterController.Move(-transform.right * m_speed * Time.deltaTime);
+            m_characterController.Move(-transform.right * (m_speed/2) * Time.deltaTime);
         }
 
         m_characterController.Move(m_gravity*Time.deltaTime);
@@ -49,24 +41,29 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // Input Actions
+    // W
     public void Forward(InputAction.CallbackContext context)
     {
         float value = context.ReadValue<float>();
         m_moveForward = value > 0;
         Debug.Log("Forward detected");
     }
+    // S
     public void Backward(InputAction.CallbackContext context)
     {
         float value = context.ReadValue<float>();
         m_moveBackward = value > 0;
         Debug.Log("Backward detected");
     }
+    // A
     public void Left(InputAction.CallbackContext context)
     {
         float value = context.ReadValue<float>();
         m_rotLeft = value > 0;
         Debug.Log("Forward detected");
     }
+    // D
     public void Right(InputAction.CallbackContext context)
     {
         float value = context.ReadValue<float>();
