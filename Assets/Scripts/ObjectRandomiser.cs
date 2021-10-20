b45ec1836c86fb232fe27c989a364aaf2bb35d32
Dan <danpaul309@gmail.com>
2021-10-20 15:14:31 +0100

    Updated object randomiser

--- Assets/Scripts/ObjectRandomiser.cs
+++ Assets/Scripts/ObjectRandomiser.cs
@@ -14,7 +14,9 @@ public class ObjectRandomiser : MonoBehaviour
     [SerializeField] private GameObject m_nessie;
     [SerializeField] private GameObject m_nessieDestination;
     [SerializeField] private GameObject m_book;
+    [SerializeField] private GameObject m_bookDestination;
     [SerializeField] private GameObject m_plate;
+    [SerializeField] private GameObject m_plateDestination;
 
     [SerializeField] private int m_noOfBooks;
     [SerializeField] private int m_noOfPlates;
@@ -25,46 +27,131 @@ public class ObjectRandomiser : MonoBehaviour
     private List<int> m_chosenNumbers = new List<int>();
 
     private int m_randomNo;
-
     private bool m_interact;
+    private float m_inputTimer = 0.0f;
+
+
+    private
+
     // Start is called before the first frame update
     void Start()
     {
-       // Randomise();
+        Randomise();
     }
 
     void Update()
     {
-        if (m_interact)
+        if (m_inputTimer != 0.0f)
         {
-           // Randomise();
+            m_inputTimer -= Time.deltaTime * 2;
+            m_inputTimer = m_inputTimer < 0.01f ? 0.0f : m_inputTimer;
+        }
+
+        if (m_interact && m_inputTimer == 0.0f)
+        {
+            m_inputTimer = 0.5f;
+            Randomise();
         }
     }
 
-   /* private void Randomise()
+    private void DestroyAllInteractables()
     {
-        Random.InitState((int)System.DateTime.Now.Ticks);
-        bool unique = false;
+        List<GameObject> m_gameObjects = GameObject.FindGameObjectsWithTag("Interactable");
+        foreach (var obj in m_gameObjects)
+        {
+            if (obj.GetComponent<TO_Basic>().m_type == TaskObject.Type.Toy)
+            {
+                Destroy(obj);
+            }
+        }
+    }
 
-        for (int i = 0; i < m_noOfNessies; i++)
+    private void SpawnObject(TaskObject.Type type)
+    {
+        int m_noOf;
+        GameObject m_object;
+        GameObject m_objectDestination;
+        List<GameObject> m_objectSpawnLocations;
+
+        switch (type)
+        {
+            case (TaskObject.Type.Toy):
+                {
+                    m_noOf = m_noOfNessies;
+                    m_object = m_nessie;
+                    m_objectDestination = m_nessieDestination;
+                    m_objectSpawnLocations = m_nessieSpawnLocations;
+                    break;
+                }
+            case (TaskObject.Type.Plate):
+                {
+                    m_noOf = m_noOfNessies;
+                    m_object = m_nessie;
+                    m_objectDestination = m_nessieDestination;
+                    m_objectSpawnLocations = m_nessieSpawnLocations;
+                    break;
+                }
+            case (TaskObject.Type.Book):
+                {
+                    m_noOf = m_noOfNessies;
+                    m_object = m_nessie;
+                    m_objectDestination = m_nessieDestination;
+                    m_objectSpawnLocations = m_nessieSpawnLocations;
+                    break;
+                }
+            default:
+                break;
+        }
+
+        m_object.GetComponent<TO_Basic>().tasksystem = m_taskSystem;
+        m_object.GetComponent<TO_Basic>().m_destination = m_objectDestination;
+
+        m_chosenNumbers.Clear();
+        bool unique = false;
+        for (int i = 0; i < m_noOf; i++)
         {
             unique = false;
             while (!unique)
             {
-                m_randomNo = Random.Range(0, m_nessieSpawnLocations.Count);
+                m_randomNo = UnityEngine.Random.Range(0, m_objectSpawnLocations.Count);
                 if (!m_chosenNumbers.Contains(m_randomNo))
                 {
                     m_chosenNumbers.Add(m_randomNo);
                     unique = true;
                 }
             }
+
+            Instantiate(m_object, m_objectSpawnLocations[m_randomNo].transform.position, Quaternion.identity);
         }
+    }
 
-        m_nessie.GetComponent<TO_Basic>().tasksystem = m_taskSystem;
-        m_nessie.GetComponent<TO_Basic>().m_destination = m_nessieDestination;
+    private void Randomise()
+    {
 
-        Instantiate(m_nessie, m_nessieSpawnLocations[m_randomNo].transform.position, Quaternion.identity);
-    }*/
+        DestroyAllInteractables();
+
+        UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks);
+
+        //bool unique = false;
+        //for (int i = 0; i < m_noOfNessies; i++)
+        //{
+        //    unique = false;
+        //    while (!unique)
+        //    {
+        //        m_randomNo = UnityEngine.Random.Range(0, m_nessieSpawnLocations.Count);
+        //        if (!m_chosenNumbers.Contains(m_randomNo))
+        //        {
+        //            m_chosenNumbers.Add(m_randomNo);
+        //            unique = true;
+        //        }
+        //    }
+        //}
+
+        //m_nessie.GetComponent<TO_Basic>().tasksystem = m_taskSystem;
+        //m_nessie.GetComponent<TO_Basic>().m_destination = m_nessieDestination;
+
+        //Instantiate(m_nessie, m_nessieSpawnLocations[m_randomNo].transform.position, Quaternion.identity);
+    }
 
     public void TestRandomise(InputAction.CallbackContext context)
     {
