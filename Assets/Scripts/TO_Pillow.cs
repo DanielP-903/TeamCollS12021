f6d937a509a83f9d4e52fc5f136a16b9e03d7d8e
Dan <44652550+DanielP-903@users.noreply.github.com>
2021-11-10 15:06:05 +0000

    Added fluffing pillow mechanic

---
+++ Assets/Scripts/TO_Pillow.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TO_Pillow : TaskObject
+{
+    private DayAndNightCycle m_dayNightCycleRef;
+    private ParticleSystem m_particleSystem;
+    void Start()
+    {
+        m_dayNightCycleRef = GameObject.FindGameObjectWithTag("DayNight Cycle").GetComponent<DayAndNightCycle>();
+        if (!TryGetComponent(out BoxCollider boxCollider))
+        {
+            Debug.LogError("No Box Collider found!");
+            Debug.DebugBreak();
+        }
+        if (!TryGetComponent(out ParticleSystem particleSystem))
+        {
+            Debug.LogError("No Particle System found!");
+            Debug.DebugBreak();
+        }
+
+        LoadAssets();
+
+        m_particleSystem = particleSystem;
+        m_particleSystem.Stop();
+
+        m_active = true;
+    }
+
+    void Update()
+    {
+        GetComponent<BoxCollider>().enabled = m_active;
+        if (m_active)
+        {
+        }
+    }
+
+    public override void Complete()
+    {
+        m_active = false;
+        m_particleSystem.Clear();
+        m_particleSystem.Play();
+        Debug.Log("Fluffed pillow!");
+        m_taskSystem.Method8();
+        //m_dayNightCycleRef.time = 2.0f;
+    }
+}
