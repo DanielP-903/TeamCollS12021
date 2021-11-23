7c638bf5ddd9c07f28ab7af4ac964cf1a5d76007
MKXScorpion <ananthpondichery62@gmail.com>
2021-11-23 12:15:51 +0000

    GameOver

--- Assets/UI/MenuDayNightCycle.cs
+++ Assets/UI/MenuDayNightCycle.cs
@@ -25,6 +25,7 @@ public class MenuDayNightCycle : MonoBehaviour
 
     public GameObject lighthouselight;
     internal GameObject pipe;
+    public int scenevalue;
 
 
 
@@ -45,15 +46,16 @@ public class MenuDayNightCycle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (scenevalue == 0)
+        {
+            time += TimeRate * Time.deltaTime;
+            time += Time.deltaTime / fullDayLength; //  total time  full day cycle
+            dayover = time % 1f;
 
-        time += TimeRate * Time.deltaTime;     
-        time += Time.deltaTime / fullDayLength; //  total time  full day cycle
-        dayover = time % 1f;
 
-   
 
-          //  Sun.transform.eulerAngles = (time - 0.25f) * circlerotation * 4.0f;  
-           // Moon.transform.eulerAngles = (time - 0.75f) * circlerotation * 4.0f;
+            //  Sun.transform.eulerAngles = (time - 0.25f) * circlerotation * 4.0f;  
+            // Moon.transform.eulerAngles = (time - 0.75f) * circlerotation * 4.0f;
 
             /*
             if (time <= 1.0f)
@@ -92,7 +94,7 @@ public class MenuDayNightCycle : MonoBehaviour
             }
             if (time >= 1.0f)                        //skye cullin night sound
             {
-                Sun.transform.eulerAngles = (time - 0.45f) * circlerotation * 2.0f;    
+                Sun.transform.eulerAngles = (time - 0.45f) * circlerotation * 2.0f;
                 Sun.intensity = MoonIntensity.Evaluate(time);
                 Sun.color = gd2.Evaluate(time);
                 if (Moon.intensity < 0 && Moon.gameObject.activeInHierarchy) // disable
@@ -101,28 +103,110 @@ public class MenuDayNightCycle : MonoBehaviour
                     Moon.gameObject.GetComponent<Light>().enabled = true;
             }
 
-            if(time>2.0f)
-        {
-            time = 0f;
-        }
+            if (time > 2.0f)
+            {
+                time = 0f;
+            }
 
-         if(time>1.5f)
-          {
-            lighthouselight.SetActive(true);
-        lighthouselight.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
-        pipe.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
-        }
-        else
-        {
-            lighthouselight.SetActive(false);
-            pipe.transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime);
-        }
+            if (time > 1.5f)
+            {
+                lighthouselight.SetActive(true);
+                lighthouselight.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+                pipe.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+            }
+            else
+            {
+                lighthouselight.SetActive(false);
+                pipe.transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime);
+            }
 
-                
+
 
 
             // lighting intensity
-           // RenderSettings.ambientIntensity = randomsunIntensity.Evaluate(time);
-          //  RenderSettings.reflectionIntensity = randommoonIntensity.Evaluate(time);
+            // RenderSettings.ambientIntensity = randomsunIntensity.Evaluate(time);
+            //  RenderSettings.reflectionIntensity = randommoonIntensity.Evaluate(time);
         }
+            if (scenevalue == 1)
+        {
+            time += TimeRate* Time.deltaTime;
+    time += Time.deltaTime / fullDayLength; //  total time  full day cycle
+            dayover = time % 1f;
+
+
+
+            //  Sun.transform.eulerAngles = (time - 0.25f) * circlerotation * 4.0f;  
+            // Moon.transform.eulerAngles = (time - 0.75f) * circlerotation * 4.0f;
+
+            /*
+            if (time <= 1.0f)
+            {
+                Sun.gameObject.SetActive(true);
+                Moon.gameObject.SetActive(false);
+                if (Sun.intensity < 0 && Sun.gameObject.activeInHierarchy)  // disable 
+                    Sun.gameObject.GetComponent<Light>().enabled = false;
+                else if (Sun.intensity > 0 && Sun.gameObject.activeInHierarchy) // enable
+                    Sun.gameObject.GetComponent<Light>().enabled = true;
+            }
+
+            if (time > 1.0f)
+            {
+                Moon.gameObject.SetActive(true);
+                Moon.gameObject.SetActive(false);
+                if (Moon.intensity < 0 && Moon.gameObject.activeInHierarchy) // disable
+                    Moon.gameObject.GetComponent<Light>().enabled = false;
+                else if (Moon.intensity > 0 && Moon.gameObject.activeInHierarchy) // enable
+                    Moon.gameObject.GetComponent<Light>().enabled = true;
+            }*/
+
+
+            // teller of tell tales morning sound
+            // pippin afternoon and evening sound
+            // Daniel change for build
+            if (time< 1.0f)
+            {
+                Sun.transform.eulerAngles = (time - 0.45f) * circlerotation* 2.0f;
+                Sun.intensity = SunIntensity.Evaluate(time);  // Light component intensity over time
+                Sun.color = gd.Evaluate(time);  // Light component color over time
+                if (Sun.intensity< 0 && Sun.gameObject.activeInHierarchy)  // disable 
+                    Sun.gameObject.GetComponent<Light>().enabled = false;
+                else if (Sun.intensity > 0 && Sun.gameObject.activeInHierarchy) // enable
+                    Sun.gameObject.GetComponent<Light>().enabled = true;
+            }
+if (time >= 1.0f)                        //skye cullin night sound
+{
+    Sun.transform.eulerAngles = (time - 0.45f) * circlerotation * 2.0f;
+    Sun.intensity = MoonIntensity.Evaluate(time);
+    Sun.color = gd2.Evaluate(time);
+    if (Moon.intensity < 0 && Moon.gameObject.activeInHierarchy) // disable
+        Moon.gameObject.GetComponent<Light>().enabled = false;
+    else if (Moon.intensity > 0 && Moon.gameObject.activeInHierarchy) // enable
+        Moon.gameObject.GetComponent<Light>().enabled = true;
+}
+
+if (time > 2.0f)
+{
+    time = 0f;
+}
+
+if (time > 0.3f)
+{
+    lighthouselight.SetActive(true);
+    lighthouselight.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+    pipe.transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+}
+else
+{
+   // lighthouselight.SetActive(false);
+   // pipe.transform.Rotate(new Vector3(0, 0, 0) * Time.deltaTime);
+}
+
+
+
+
+            // lighting intensity
+            // RenderSettings.ambientIntensity = randomsunIntensity.Evaluate(time);
+            //  RenderSettings.reflectionIntensity = randommoonIntensity.Evaluate(time);
+        }
+    }
     }
